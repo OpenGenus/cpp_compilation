149db0359815210e66f2dc7e53586c3dd1c35bbe
Harshita Sahai <40337803+HarshitaSahai@users.noreply.github.com>
2019-07-02 18:50:04 +0530

    Create cpp1.cpp

---
+++ cpp1.cpp
@@ -0,0 +1,10 @@
+#include <iostream> //pre processor
+#define newline std::cout<<"\n"; //pre processor
+
+int main()
+{
+      std::cout<<"The text in first line.";
+      newline
+      std::cout<<"The text in the next line.";
+      return 0;
+}
